a01ce43b124acee8ac42fe9b679d0073e5557356
Dibyakanti Layek <dibyakanti095@gmail.com>
2022-04-16 21:03:12 +0530

    myt

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -1,5 +1,5 @@
 const authorModel = require('../models/authorModel')
- const bookModel = require('../models/bookModel')
+//  const bookModel2 = require('../models/bookModel2')
 
 const createAuthor = async function(req, res){
     const body = req.body;
@@ -11,31 +11,32 @@ const createAuthor = async function(req, res){
      }
 }
 
+
 const createNewBook = async function (req, res){
     const reqBook = req.body;
-    const Saved = await bookModel.create(reqBook)
+    const Saved = await bookModel2.create(reqBook)
     res.send({msg: Saved})
 }
 
 const allBooks = async function(req, res) {
     const authorDetails = await authorModel.find({author_name: "Chetan Bhagat"})
     const id = authorDetails[0].autor_id
-    const booksName = await bookModel.find({author_id: id}).select({name:1})
+    const booksName = await bookModel2.find({author_id: id}).select({name:1})
     res.send({msg:booksName})
 }
 
 const upadatedBookPrice = async function(req, res) {
-    const bookDetails = await bookModel.find({name:"Two states"})
+    const bookDetails = await bookModel2.find({name:"Two states"})
     const id = bookDetails[0].author_id
     const authorN = await authorModel.find({author_id:id}).select({author_name:1, _id:0})
     const bkName = bookDetails[0].name
-    const updatedPrice = await bookModel.findByIdAndUpdate({name:bkName}, {price:100},{new:true}).select({price:1, _id:0})
+    const updatedPrice = await bookModel2.findByIdAndUpdate({name:bkName}, {price:100},{new:true}).select({price:1, _id:0})
 
     res.send({msg: authorN, updatedPrice})
 }
 
 const authorsname = async function(req, res){
-    const booksId = await bookModel.find({price: {$gte:50, $lte:100,}}).select({author_id:1, _id:0})
+    const booksId = await bookModel2.find({price: {$gte:50, $lte:100,}}).select({author_id:1, _id:0})
     const id = booksId.map(inp => inp.author_id)
 
     let temp =[]
@@ -50,7 +51,6 @@ const authorsname = async function(req, res){
 
     res.send({msg:authorName})
 }
-
 module.exports.createAuthor = createAuthor
  module.exports.createNewBook = createNewBook
  module.exports.allBooks = allBooks
