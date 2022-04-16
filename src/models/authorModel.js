9bd00854d48dcee52106deeaabe463c07dbfd5ff
Dibyakanti Layek <dibyakanti095@gmail.com>
2022-04-16 21:04:26 +0530

    ki

--- src/models/authorModel.js
+++ src/models/authorModel.js
@@ -1,19 +1,14 @@
 const mongoose = require('mongoose');
 
 const authorSchema = new mongoose.Schema( {
-    author_id: Number,
-    author_name: String,
+    author_id: {
+        type: Number,
+        required: true
+    },
+    authorname: String,
     age: Number,
     address: String,
 } , {timestemps: true});
-
-// const booksScheme = new mongoose.Schema( {
-//     name: String,
-//     author_id: Number,
-//     price: Number,
-//     ratings: Number,
-// } , { timestemps: true});
-
 
 module.exports = mongoose.model('Author' , authorSchema)
 // module.exports = mongoose.model('Book', booksScheme)
